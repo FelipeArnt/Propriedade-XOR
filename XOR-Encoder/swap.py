9f29529bf3eab5f18d9af888e1f0cef317b37a39
FelipeArnt <felipe.arnt@edu.pucrs.br>
2025-12-27 22:21:12 -0300

    add files

--- XOR-Encoder/swap.py
+++ XOR-Encoder/swap.py
@@ -1,21 +1,23 @@
-print("Propriedade XOR -> a^b^b == a")
-
-print([(a, b) for a in range(2) for b in range(2)])
-
-print([a ^ b ^ b == a for a in range(2) for b in range(2)])
-
-print("\n")
-
-m = "Felipe Arnt"
-k = 256
-
-n = [chr(ord(a)) for a in m]
-
-print("Texto pleno: \n")
-print(n)
-
-print("\nCriptografado: ")
 
 
-cript = [chr(ord(a) ^ k) for a in m]
-print(cript)
+def hello():
+
+    print("Propriedade XOR -> a^b^b == a")
+    print([(a, b) for a in range(2) for b in range(2)])
+    print([a ^ b ^ b == a for a in range(2) for b in range(2)])
+    print("\n")
+
+def xorEncoder(m,k):
+    n = [chr(ord(a)) for a in m]
+
+    print("Texto pleno: \n")
+    print(n)
+
+    print("\nCriptografado: ")
+
+    cript = [chr(ord(a) ^ k) for a in m]
+    print(cript)
+
+if __name__ == "__main__":
+    hello()
+    xorEncoder(m="hello", k=256)
